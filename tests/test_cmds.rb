cad884b9ef96a272afd0cea5beec72fac0b0e539
Andrey Blinov <andrey.blinoff@gmail.com>
2016-04-14 12:27:13 +0300

    convert to spaces

--- tests/test_cmds.rb
+++ tests/test_cmds.rb
@@ -2,18 +2,18 @@ require_relative 'test_base'
 
 class TestCmds < TestBase
 
-	def test_mget
-		@rc.set('test1', 'test1')
-		@rc.set('test2', 'test2')
-		@rc.set('test3', 'test3')
-		@rc.set('test4', 'test4')
+  def test_mget
+    @rc.set('test1', 'test1')
+    @rc.set('test2', 'test2')
+    @rc.set('test3', 'test3')
+    @rc.set('test4', 'test4')
 
-		assert_equal(['test2', 'test1', 'test3'], @rc.mget(['test2', 'test1', 'test3']))
-		assert_equal(['test2', 'test1', 'test3'], @rc.mget('test2', 'test1', 'test3'))
+    assert_equal(['test2', 'test1', 'test3'], @rc.mget(['test2', 'test1', 'test3']))
+    assert_equal(['test2', 'test1', 'test3'], @rc.mget('test2', 'test1', 'test3'))
 
-		assert_equal(['test2', 'test1', 'test3', ''], @rc.mget(['test2', 'test1', 'test3', 'test5']))
-		assert_equal(['test2', 'test1', 'test3', nil], @rc.mget(['test2', 'test1', 'test3', nil]))
-		
-	end
+    assert_equal(['test2', 'test1', 'test3', ''], @rc.mget(['test2', 'test1', 'test3', 'test5']))
+    assert_equal(['test2', 'test1', 'test3', nil], @rc.mget(['test2', 'test1', 'test3', nil]))
+    
+  end
 
 end
